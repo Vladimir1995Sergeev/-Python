ef64000c2c0009ccc3487ee3efd5a4a82e32cbd7
Vladimir1995Sergeev <96547361+Vladimir1995Sergeev@users.noreply.github.com>
2022-01-27 19:36:01 +0300

    Add files via upload

--- lesson1.py
+++ lesson1.py
@@ -1,8 +1,21 @@
-def simple_calc():
-    a = float(input("work_in_hours : "))
-    b = float(input("rate_per_hour : "))
-    c = float(input("prize : "))
-    pay = a * b
-    return pay + c
+class Matrix:
+    def __init__(self, my_list):
+        self.my_list = my_list
 
-print(f"Wages are paid : {simple_calc()}" )
+    def __str__(self):
+        for row in self.my_list:
+            for el in row:
+                print(f"{el:4}", end="")
+            print()
+        return ''
+
+    def __add__(self, other):
+        for el in range(len(self.my_list)):
+            for el_2 in range(len(other.my_list[el])):
+                self.my_list[el][el_2] = self.my_list[el][el_2] + other.my_list[el][el_2]
+        return Matrix.__str__(self)
+
+
+m = Matrix([[-1, 0, 1], [-1, 0, 1], [0, 1, -1], [1, 1, -1]])
+new_m = Matrix([[-2, 0, 2], [-2, 0, 2], [0, 2, -2], [2, 2, -7]])
+print(m.__add__(new_m))
